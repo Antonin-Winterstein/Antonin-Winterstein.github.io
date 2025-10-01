400e89284097b2e52cff9d1f5dbca1199ef2c339
antonin-winterstein <awinterstein@allinbox.fr>
2025-10-01 15:58:49 +0200

    Testing

--- AIBSDKWorker.js
+++ AIBSDKWorker.js
@@ -1,1 +1,4 @@
-importScripts("https://d3hj16ga6b.sharedwithexpose.com/sdk/webpush/v1/AIB.sw.js?v=2");
+// Générer une version dynamique (ex: timestamp)
+const version = Date.now();
+
+importScripts(`https://d3hj16ga6b.sharedwithexpose.com/sdk/webpush/v1/AIB.sw.js?v=${version}`);
